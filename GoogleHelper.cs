46abca629eb70aaa86bd01b29372608682275024
TalonEzio <vuthemanh1707@gmail.com>
2024-02-07 10:22:32 +0700

    Add project files.

---
+++ GoogleHelper.cs
@@ -0,0 +1,51 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v2;
+
+
+namespace BlockDownloadGoogleDrive
+{
+    internal class GoogleHelper
+    {
+        internal static async Task<UserCredential> Login(string clientId, string clientSecret, string[] scopes)
+        {
+            ClientSecrets clientSecrets = new ClientSecrets()
+            {
+                ClientId = clientId,
+                ClientSecret = clientSecret
+            };
+            return await GoogleWebAuthorizationBroker.AuthorizeAsync(clientSecrets, scopes, "user", CancellationToken.None);
+        }
+        internal static async Task BlockDownloadWithFolderId(DriveService driveService, string folderId)
+        {
+            string folderMimeType = "application/vnd.google-apps.folder";
+
+            try
+            {
+                var request = driveService.Files.List();
+                request.Q = $"parents in '{folderId}'";
+
+                var fileRequest = await request.ExecuteAsync();
+                foreach (var file in fileRequest.Items)
+                {
+                    if (!file.MimeType.Equals(folderMimeType))
+                    {
+                        Console.WriteLine($"Đã đọc được file {file.Title}, chuẩn bị chặn donwload");
+                        file.Labels.Restricted = true;
+                        file.CopyRequiresWriterPermission = true;
+                        driveService.Files.Update(file, file.Id).Execute();
+                        await Console.Out.WriteLineAsync($"Chặn thành công id : {file.Id}");
+                    }
+                    else
+                    {
+                        await Console.Out.WriteLineAsync($"Đã đọc được folder {file.Id}");
+                        await BlockDownloadWithFolderId(driveService, file.Id);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Google.Apis.Drive.v2;
+using Google.Apis.Services;
+using System.Text;
+
+namespace BlockDownloadGoogleDrive
+{
+    internal class Program
+    {
+        private static string clientId = "597540699804-nd7rmi77jt0fjvmkh3pfhdrduv0ct7bv.apps.googleusercontent.com";
+        private static string clientSecret = "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";
+
+        private static string[] scopes = new[] { "https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.file" };
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.InputEncoding = Console.OutputEncoding = Encoding.Unicode;
+                var userCredential = await GoogleHelper.Login(clientId, clientSecret, scopes);
+                using var driveService = new DriveService(
+                    new BaseClientService.Initializer() { HttpClientInitializer = userCredential }
+                    );
+
+                await Console.Out.WriteAsync("FolderId: ");
+                string folderId = Console.ReadLine() ?? String.Empty;
+                await GoogleHelper.BlockDownloadWithFolderId(driveService, folderId);
+                await Console.Out.WriteLineAsync("Hoàn tất");
+            }
+            catch (Exception e)
+            {
+
+                await Console.Out.WriteLineAsync(e.Message);
+                Console.ReadLine();
+            }
+
+        }
+
+    }
+}
